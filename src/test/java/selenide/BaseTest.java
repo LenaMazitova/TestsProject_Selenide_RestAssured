0148389459e06d92cf059837779dd7a4a779b9c5
LenaMazitova <0107lenam@gmail.com>
2024-11-02 18:07:55 +0200

    Исправлено для Testcontainers (без теста со скачиванием файла)

--- src/test/java/selenide/BaseTest.java
+++ src/test/java/selenide/BaseTest.java
@@ -2,21 +2,83 @@ package selenide;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.FileDetector;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.env.Environment;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+@Testcontainers
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BaseTest {
 
-    @BeforeAll
-    static void setUp(){
-        Configuration.timeout = 8000;
-//        default = "1366x768"
-        Configuration.browserSize = "1920x1080";
+//    public static final File RECORDINGS_DIR = new File("target/selenide");
+
+    // Проверяем, доступен ли Docker
+    public static boolean isDockerAvailable() {
+        try {
+            return Runtime.getRuntime().exec("docker info").waitFor() == 0;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
+    @Container
+    public static BrowserWebDriverContainer<?> webDriverContainer =
+            new BrowserWebDriverContainer<>(
+                    System.getProperty("os.arch").equals("aarch64")
+                            ? DockerImageName.parse("seleniarm/standalone-chromium")
+                            .asCompatibleSubstituteFor("selenium/standalone-chrome")
+                            : DockerImageName.parse("selenium/standalone-chrome"))
+                    .withCapabilities(
+                            new ChromeOptions()
+                                    .addArguments("--no-sandbox")
+                                    .addArguments("--disable-dev-shm-usage")
+                                    .addArguments("download.default_directory=/home/seluser/Downloads")
+                                    .addArguments("download.prompt_for_download=false")
+                                    .addArguments("safebrowsing.enabled")
+                    );
+//                    .withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.RECORD_FAILING,
+//                            RECORDINGS_DIR);
+
     @AfterAll
-    static void tearDown(){
-        Selenide.closeWebDriver();
+    static void tearDown() {
+        if (webDriverContainer != null) {
+            webDriverContainer.stop();
+        }
+//        Selenide.closeWebDriver();
     }
+    @BeforeAll
+    static void beforeAll() {
+        webDriverContainer.start();
+        RemoteWebDriver remoteWebDriver = webDriverContainer.getWebDriver();
+        WebDriverRunner.setWebDriver(remoteWebDriver);
+
+//        Configuration.downloadsFolder = "/home/seluser/Downloads";
+
+//        Configuration.reportsFolder = "target/selenide";
+
+        Configuration.timeout = 8000;
+        Configuration.browserSize = "1920x1080";  // default = "1366x768"
+    }
+
 
 }
--- src/test/java/selenide/SelenideTests.java
+++ src/test/java/selenide/SelenideTests.java
@@ -1,13 +1,22 @@
 package selenide;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Ignore;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.condition.DisabledIf;
+import org.testcontainers.containers.BrowserWebDriverContainer;
 import selenide.pages.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.nio.file.Files;
 import java.util.List;
+
 
 import static com.codeborne.selenide.Selenide.switchTo;
 import static org.junit.jupiter.api.Assertions.*;
@@ -53,6 +62,7 @@ class SelenideTests extends BaseTest {
                 .openContactsWindow()
                 .openContactsPage();
         String region = sbisContactsPage.getDefaultRegion();
+
         assertEquals("Калининградская обл.", region);
         assertTrue(sbisContactsPage.isContactsListExists());
 
@@ -67,15 +77,20 @@ class SelenideTests extends BaseTest {
     }
 
     @Test
-    public void scenario3() {
+    @DisabledIf("isTestcontainersLaunch")
+    public void scenario3() throws IOException {
         SbisPage sbisPage = new SbisPage();
         DownloadPage downloadPage = sbisPage.downloadPlugin();
+
         File file = downloadPage.downloadWebInstaller();
+
         assertEquals("sbisplugin-setup-web.exe", file.getName());
 
         Double expectedFileLength = downloadPage.getFileLengthMegaBytes();
         Double actualFileLength = bytesToMegabytes(file.length());
         assertEquals(expectedFileLength, actualFileLength);
+
+        Files.delete(file.toPath());
     }
 
     public double bytesToMegabytes(long bytes) {
@@ -83,4 +98,34 @@ class SelenideTests extends BaseTest {
                 .divide(BigDecimal.valueOf(1024 * 1024), 2, RoundingMode.HALF_UP);
         return megabytes.doubleValue();
     }
+    boolean isTestcontainersLaunch() {
+        // Проверяемое системное свойство
+        return Boolean.getBoolean("skip.test");
+    }
+
+//    static void checkContainer() {
+//        if (isDockerAvailable()){
+//            try {
+//                String output = executeCommandInContainer(webDriverContainer,
+//                        "mkdir -p /home/seluser/Downloads && chmod 777 /home/seluser/Downloads");
+//                System.out.println("Результат выполнения команд:\n" + output);
+//            } catch (Exception e) {
+//                System.out.println("Ошибка" + e.getMessage());
+//            }
+//        }
+//    }
+//
+//    static String executeCommandInContainer(BrowserWebDriverContainer<?> container, String command) throws IOException, InterruptedException {
+//
+//        // Выполнение команды внутри контейнера
+//        String[] cmd = {"/bin/sh", "-c", command}; // Используем shell для выполнения команд
+//        var execResult = container.execInContainer(cmd); // Выполнение команды и получение результата
+//
+//        // Проверка на наличие ошибок
+//        if (execResult.getExitCode() != 0) {
+//            throw new IOException("Команда завершилась с ошибкой: " + execResult.getStderr());
+//        }
+//        // Возврат стандартного вывода команды
+//        return execResult.getStdout();
+//    }
 }
